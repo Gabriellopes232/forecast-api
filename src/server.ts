26a46cdfa05f0defea725068fca02e79aa239803
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-16 10:20:13 -0300

    feat: create methods express setup

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import { Application } from 'express';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+
+export class SetupServer extends Server {
+
+    constructor(private port = 3000) {
+        super();
+    }
+
+    public async init(): Promise<void> {
+        this.setupExpress();
+        this.setupControllers();
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+        this.setupControllers();
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+        this.addControllers([forecastController]);
+    }
+
+    public getApp(): Application {
+        return this.app;
+    }
+}
