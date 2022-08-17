83dd29bdb6f32a9f440632646038d891d8bb06ce
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-17 09:19:55 -0300

    feat: test unit get route from api

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+
+jest.mock('axios')
+
+describe('StormGlass client', () => {
+    it('should return the normalized forecast from the StormGlass service', async () => {
+        const lat = -33.7972868;
+        const lng = 151.289824
+
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+
+        const stormGlass = new StormGlass(axios)
+        const response = await stormGlass.fetchPoints(lat, lng);
+        expect(response).toEqual(stormGlassNormalized3HoursFixture)
+    })
+})
