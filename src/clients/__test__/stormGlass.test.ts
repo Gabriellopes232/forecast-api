1f7b0b04b0003251a1aa832aacc8d2aee51212b5
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-19 08:45:00 -0300

    feat: test checked

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -10,7 +10,7 @@ describe('StormGlass client', () => {
         const lat = -33.7972868;
         const lng = 151.289824
 
-        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
         const stormGlass = new StormGlass(axios)
         const response = await stormGlass.fetchPoints(lat, lng);
