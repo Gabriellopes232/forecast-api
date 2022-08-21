68699b3dbb65286eab04a34aefd18d58f5dafcf4
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-21 11:27:18 -0300

    Revert "feat: using jest with typescript"
    This reverts commit 63a6401aa85f7537e131417daa0e7a1ec12771be.

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,14 +6,13 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios')
 
 describe('StormGlass client', () => {
-    const mockedAxios = axios as jest.Mocked<typeof axios>
     it('should return the normalized forecast from the StormGlass service', async () => {
-        const lat = -33.7972868
+        const lat = -33.7972868;
         const lng = 151.289824
 
-        mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-        const stormGlass = new StormGlass(mockedAxios)
+        const stormGlass = new StormGlass(axios)
         const response = await stormGlass.fetchPoints(lat, lng);
         expect(response).toEqual(stormGlassNormalized3HoursFixture)
     })
