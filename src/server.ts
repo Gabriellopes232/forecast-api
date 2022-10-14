dd538f8e92e9a5412f986f388f59318fb503825d
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-10-14 14:47:19 -0300

    feat: add new changes in server

--- src/server.ts
+++ src/server.ts
@@ -5,26 +5,34 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
-    constructor(private port = 3000) {
-        super();
-    }
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public async init(): Promise<void> {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-        this.setupControllers();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.setupControllers();
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
