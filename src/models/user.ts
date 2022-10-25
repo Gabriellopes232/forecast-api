61585b631a2927ae0d61495c5f9c9949269c5312
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-10-25 16:37:06 -0300

    feat: add auth service

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,4 @@
+import AuthService from '@src/services/auth';
 import mongoose, { Document, Model } from 'mongoose';
 
 export interface User {
@@ -41,5 +42,18 @@ schema.path('email').validate(
         const emailCount = await mongoose.models.User.countDocuments({ email });
         return !emailCount;
     }, 'already exists in the database.', CUSTOM_VALIDATION.DUPLICATED);
+
+schema.pre('save', async function (): Promise<void> {
+    if (!this.password || this.isModified('password')) {
+        return
+    }
+
+    try {
+        const hashedPassword = await AuthService.hashPassword(this.password)
+        this.password = hashedPassword
+    } catch (error) {
+        console.error(`Error hashing the password for the user ${this.name}`)
+    }
+})
 
 export const User: Model<UserModel> = mongoose.model<UserModel>('User', schema);
---
+++ src/services/auth.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+
+export default class AuthService {
+    public static async hashPassword(
+        password: string,
+        salt = 10): Promise<string> {
+        return await bcrypt.hash(password, salt)
+    }
+
+    public static async comparePasswords(
+        hashedPassword: string,
+        password: string): Promise<boolean> {
+        return await bcrypt.compare(password, hashedPassword)
+    }
+}
