424028e636bd2b503eacd61f747bc8ba2e6d94c7
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-10-26 15:40:41 -0300

    feat: create method to generate token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,15 +1,25 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
-    public static async hashPassword(
-        password: string,
-        salt = 10): Promise<string> {
-        return await bcrypt.hash(password, salt)
-    }
+  public static async hashPassword(
+    password: string,
+    salt = 10
+  ): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
 
-    public static async comparePasswords(
-        hashedPassword: string,
-        password: string): Promise<boolean> {
-        return await bcrypt.compare(password, hashedPassword)
-    }
+  public static async comparePasswords(
+    hashedPassword: string,
+    password: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static generateToken(payload: object): string {
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn'),
+    });
+  }
 }
