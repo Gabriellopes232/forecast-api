b98cee78a35f5c4c30c25b5009450646a6c31ca5
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-02-08 12:40:08 -0300

    feat: implements cache util

---
+++ src/util/cache.ts
@@ -0,0 +1,19 @@
+import NodeCache from 'node-cache';
+
+class CacheUtil {
+  constructor(protected cacheService = new NodeCache()) {}
+
+  public set<T>(key: string, value: T, ttl = 3600): boolean {
+    return this.cacheService.set(key, value, ttl);
+  }
+
+  public get<T>(key: string): T | undefined {
+    return this.cacheService.get<T>(key);
+  }
+
+  public clearAllCache(): void {
+    return this.cacheService.flushAll();
+  }
+}
+
+export default new CacheUtil();
